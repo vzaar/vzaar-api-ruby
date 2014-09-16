f0fbb3798066b002e89007dd5e4028fd00d22601
Alan Oliver <alanoliver@gmail.com>
2014-09-16 11:18:24 +0100

    Regression test in signature API method

---
+++ examples/signature_spec.rb
@@ -0,0 +1,42 @@
+require_relative './spec_helper'
+
+describe "Signature" do
+  describe "signature" do
+    context "when user is unauthenticated" do
+      it_behaves_like "Unauthenticated", -> (api) { api.signature }
+    end
+
+    context "with no :sucess_action_redirect" do
+      it "should be successful" do
+        api = _api(login: user1["login"],
+                   application_token: user1["rw_token"])
+
+        expect(api.signature.http_status_code).to eq(200)
+      end
+    end
+
+    context "with unencoded :success_action_redirect" do
+      it "should be successful" do
+        api = _api(login: user1["login"],
+                   application_token: user1["rw_token"])
+        expect(
+          api.signature(
+            :success_action_redirect => "http://test.com"
+          ).http_status_code
+        ).to eq(200)
+      end
+    end
+
+    context "with URI encoded :success_action_redirect" do
+      it "should be successful" do
+        api = _api(login: user1["login"],
+                   application_token: user1["rw_token"])
+        expect(
+          api.signature(
+            :success_action_redirect => CGI.escape("http://test.com")
+          ).http_status_code
+        ).to eq(200)
+      end
+    end
+  end
+end
