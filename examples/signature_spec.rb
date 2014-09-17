4e1c11c46598a6e98d33c1275310e661127939e1
Alan Oliver <alanoliver@gmail.com>
2014-09-17 18:21:18 +0100

    Add more examples for signature call

--- examples/signature_spec.rb
+++ examples/signature_spec.rb
@@ -38,5 +38,29 @@ describe "Signature" do
         ).to eq(200)
       end
     end
+
+    context "encoded :success_action_redirect with qs parameters" do
+      it "should be successful" do
+        api = _api(login: user1["login"],
+                   application_token: user1["rw_token"])
+        expect(
+          api.signature(
+            :success_action_redirect => CGI.escape("http://test.com?x=y")
+          ).http_status_code
+        ).to eq(200)
+      end
+    end
+
+    context "with a :success_action_redirect with qs parameters" do
+      it "should be successful" do
+        api = _api(login: user1["login"],
+                   application_token: user1["rw_token"])
+        expect(
+          api.signature(
+            :success_action_redirect => "http://test.com?x"
+          ).http_status_code
+        ).to eq(200)
+      end
+    end
   end
 end
