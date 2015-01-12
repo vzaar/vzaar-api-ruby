3151e2839002ea1a5f2a9b346ea3e58777182fd2
Jozef Chraplewski <jozef@vzaar.com>
2015-01-12 18:10:22 +0100

    specs for API#delete/edit verbs added

--- examples/delete_video_spec.rb
+++ examples/delete_video_spec.rb
@@ -2,48 +2,116 @@ require_relative './spec_helper'
 
 describe "Delete Video" do
   vid_id = nil
+  file_path = "./spec/support/video.mov"
+  desc = "Delete Video"
 
-  before(:all) do
-    file_path = "./spec/support/video.mov"
-    desc = "Delete Video"
-    @api = _api(login: user1["login"],
-               application_token: user1["rw_token"])
-
-    @title = "api-test-#{rand_str}"
-
-    res = @api.upload_video(path: file_path, title: @title, description: desc)
-    vid_id = res.id
-  end
-
-  context "when user is unauthenticated" do
-    it_behaves_like "Unauthenticated", -> (api) do
-      api.delete_video(vid_id)
+  describe "POST verb" do
+    before(:all) do
+      @api = _api(login: user1["login"],
+                  application_token: user1["rw_token"])
+      
+      title = "api-test-#{rand_str}"
+      res = @api.upload_video(path: file_path, title: title, description: desc)
+      @vid_id = res.id
     end
-  end
 
-  context "Authenticated User" do
-    context "different account" do
-      specify do
-        api = _api(login: user2["login"],
-                   application_token: user2["rw_token"])
-        expect do
-          api.delete_video(vid_id)
-        end.to raise_error(Vzaar::Error, "Protected Resource")
+    context "when there is no method=delete param" do
+      describe "xml" do
+        specify do
+          opts = { video_id: @vid_id, http_verb: :post}
+          req = Vzaar::Request::DeleteVideo.new(@api.conn, opts)
+          
+          expect { req.execute }.to raise_error(Vzaar::Error)
+        end
+      end
+
+      describe "json" do
+        specify do
+          opts = { video_id: @vid_id, http_verb: :post, format: :json}
+          req = Vzaar::Request::DeleteVideo.new(@api.conn, opts)
+          
+          expect { req.execute }.to raise_error(Vzaar::Error)
+        end
       end
     end
 
-    context "RW token" do
-      before(:all) do
-        @res = @api.delete_video(vid_id)
+    context "when method=delete exists in post body" do
+      describe "xml" do
+        specify do
+          opts = { video_id: @vid_id, http_verb: :post}
+          req = Vzaar::Request::DeleteVideo.new(@api.conn, opts)
+
+          def req.xml_body
+            '<?xml version="1.0" encoding="UTF-8"?><vzaar-api><_method>delete</_method></vzaar-api>'
+          end
+          res = req.execute
+          expect(res.http_status_code).to eq(200)
+        end
       end
 
-      specify { expect(@res.http_status_code).to eq 200 }
-      specify { expect(@res.title).to eq(@title) }
+      describe "json" do
+        specify do
+          title = "api-test-#{rand_str}"
+          res = @api.upload_video(path: file_path, title: title, description: desc)
+          vid_id = res.id
+
+          
+          opts = { video_id: vid_id, http_verb: :post, format: :json}
+          req = Vzaar::Request::DeleteVideo.new(@api.conn, opts)
+
+          def req.json_body
+            { "vzaar-api" => { "_method" => "delete" } }
+          end
+
+          res = req.execute
+          expect(res["title"]).to eq(title)
+        end
+      end
     end
 
-    context "RO token" do
-      it_behaves_like "RO only", user1["login"], user1["ro_token"], -> (api) do
+  end
+
+  describe "DELETE verb" do
+    before(:all) do
+      @api = _api(login: user1["login"],
+                  application_token: user1["rw_token"])
+
+      @title = "api-test-#{rand_str}"
+
+      res = @api.upload_video(path: file_path, title: @title, description: desc)
+      vid_id = res.id
+    end
+
+    context "when user is unauthenticated" do
+      it_behaves_like "Unauthenticated", -> (api) do
         api.delete_video(vid_id)
+      end
+    end
+
+    context "Authenticated User" do
+      context "different account" do
+        specify do
+          api = _api(login: user2["login"],
+                     application_token: user2["rw_token"])
+          expect do
+            api.delete_video(vid_id)
+          end.to raise_error(Vzaar::Error, "Protected Resource")
+        end
+      end
+
+      context "RW token" do
+        before(:all) do
+          @res = @api.delete_video(vid_id)
+        end
+
+        specify { expect(@res.http_status_code).to eq 200 }
+        specify { expect(@res.title).to eq(@title) }
+      end
+
+      context "RO token" do
+        it_behaves_like "RO only", user1["login"], user1["ro_token"], -> (api) do
+          api.delete_video(vid_id)
+        end
       end
     end
   end
--- examples/edit_video_spec.rb
+++ examples/edit_video_spec.rb
@@ -1,6 +1,8 @@
 require_relative './spec_helper'
 
 describe "Edit Video" do
+  file_path = "./spec/support/video.mov"
+  
   context "when user is unauthenticated" do
     it_behaves_like "Unauthenticated", -> (api) do
       api.edit_video(test_video_id("user1"), title: "foo")
@@ -8,42 +10,87 @@ describe "Edit Video" do
   end
 
   context "Authenticated User" do
-    context "different account" do
-      specify do
-        api = _api(login: user2["login"],
-                   application_token: user2["rw_token"])
-
-        expect do
-          api.edit_video(test_video_id("user1"))
-        end.to raise_error(Vzaar::Error, "Moved Temporarily")
-      end
-    end
-
-    context "RW token" do
+    describe "POST verb" do
       before(:all) do
         @api = _api(login: user1["login"],
-                    application_token: user1["rw_token"])
+                    application_token: user1["rw_token"])        
+        @vid_id = test_video_id("user1")
       end
 
-      describe "updating params" do
-        before(:all) do
-          @title = rand_str()
-          @desc = rand_str()
-          @res = @api.edit_video(test_video_id("user1"),
-                                 title: @title,
-                                 description: @desc)
+      context "when there is no method=put param" do
+        describe "xml" do
+          specify do
+            opts = { video_id: @vid_id, http_verb: :post }
+            req = Vzaar::Request::EditVideo.new(@api.conn, opts)
+            
+            expect { req.execute }.to raise_error(Vzaar::Error)
+          end
+        end        
+      end
+
+      context "when method=put exists in post body" do
+        describe "xml" do
+          specify do
+            opts = { video_id: @vid_id, http_verb: :post}
+            req = Vzaar::Request::EditVideo.new(@api.conn, opts)
+
+            def req.xml_body
+              <<-XML
+                <?xml version="1.0" encoding="UTF-8"?>
+                  <vzaar-api>
+                    <_method>put</_method>
+                    <video>
+                      <title>woof</title>
+                    </video>
+                  </vzaar-api>
+                XML
+            end
+            
+            res = req.execute
+            expect(res.http_status_code).to eq(200)
+          end
         end
-
-        it_behaves_like "200 OK"
-
-        specify { expect(@res.title).to eq(@title) }
-        specify { expect(@res.description).to eq(@desc) }
-      end
+      end      
     end
 
-    context "RO token" do
-      it_behaves_like "RO only", user1["login"], user1["ro_token"], -> (api) do
-        api.edit_video(test_video_id("user1"), title: "woof")
+    describe "PUT verb" do
+      context "different account" do
+        specify do
+          api = _api(login: user2["login"],
+                     application_token: user2["rw_token"])
+
+          expect do
+            api.edit_video(test_video_id("user1"))
+          end.to raise_error(Vzaar::Error, "Moved Temporarily")
+        end
+      end
+
+      context "RW token" do
+        before(:all) do
+          @api = _api(login: user1["login"],
+                      application_token: user1["rw_token"])
+        end
+
+        describe "updating params" do
+          before(:all) do
+            @title = rand_str()
+            @desc = rand_str()
+            @res = @api.edit_video(test_video_id("user1"),
+                                   title: @title,
+                                   description: @desc)
+          end
+
+          it_behaves_like "200 OK"
+
+          specify { expect(@res.title).to eq(@title) }
+          specify { expect(@res.description).to eq(@desc) }
+        end
+      end
+
+      context "RO token" do
+        it_behaves_like "RO only", user1["login"], user1["ro_token"], -> (api) do
+          api.edit_video(test_video_id("user1"), title: "woof")
+        end
       end
     end
   end
