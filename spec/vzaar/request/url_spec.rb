b62b65544a9606a5be87da8234c911a15c912721
Ed James <ed.james.email@gmail.com>
2016-04-18 16:45:48 +0100

    (DEV-202) handle unescaped ampersands in filename

--- spec/vzaar/request/url_spec.rb
+++ spec/vzaar/request/url_spec.rb
@@ -2,24 +2,31 @@ require 'spec_helper'
 require './lib/vzaar/request/url'
 
 describe Vzaar::Request::Url do
-  let(:params) do
-    { foo: 1, bar: 2 }
-  end
-
   let(:format) { :xml }
-
   let(:url) { "/api/endpoint" }
 
   subject { described_class.new(url, format, params) }
 
   describe "#build" do
+    let(:result) { subject.build }
+
     context "when there are params" do
-      specify { expect(subject.build).to eq("/api/endpoint.xml?foo=1&bar=2") }
+      let(:params) { { foo: 1, bar: 2 } }
+      let(:expected_result) { "/api/endpoint.xml?foo=1&bar=2" }
+      specify { expect(result).to eq expected_result }
+
+      context 'and the params contain an unescaped ampersand' do
+        let(:params) { { foo: 'this & that.mp4', bar: 2 } }
+        let(:encoded_params) { URI.encode_www_form params }
+        let(:expected_result) { "/api/endpoint.xml?#{encoded_params}" }
+        specify { expect(result).to eq expected_result }
+      end
     end
 
     context "when there are params" do
       let(:params) {{}}
-      specify { expect(subject.build).to eq("/api/endpoint.xml") }
+      let(:expected_result) { "/api/endpoint.xml" }
+      specify { expect(result).to eq expected_result }
     end
   end
 end
