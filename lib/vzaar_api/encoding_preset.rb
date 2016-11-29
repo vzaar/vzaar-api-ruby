edb6522017a434f267fa47a84c05be910b14ed34
Ed James <ed.james.email@gmail.com>
2016-11-29 12:53:59 +0000

    (DEV-466) update encoding-preset attributes

--- lib/vzaar_api/encoding_preset.rb
+++ lib/vzaar_api/encoding_preset.rb
@@ -9,9 +9,9 @@ module VzaarApi
     ENDPOINT = 'encoding_presets'
 
     attr_reader :id, :name, :description, :output_format, :bitrate_kbps,
-      :long_dimension, :video_codec, :profile, :frame_rate, :keyframe, :audio_bitrate_kbps,
-      :audio_channels, :audio_sample_rate, :max_bitrate_kbps, :keyframe_period,
-      :created_at, :updated_at
+      :long_dimension, :video_codec, :profile, :frame_rate_upper_threshold,
+      :keyframe_upper_threshold, :audio_bitrate_kbps, :audio_channels,
+      :audio_sample_rate, :max_bitrate_kbps, :created_at, :updated_at
 
     def initialize(attrs = {})
       @id = attrs[:id]
@@ -19,16 +19,15 @@ module VzaarApi
       @description = attrs[:description]
       @output_format = attrs[:output_format]
       @bitrate_kbps = attrs[:bitrate_kbps]
+      @max_bitrate_kbps = attrs[:max_bitrate_kbps]
       @long_dimension = attrs[:long_dimension]
       @video_codec = attrs[:video_codec]
       @profile = attrs[:profile]
-      @frame_rate = attrs[:frame_rate]
-      @keyframe = attrs[:keyframe]
+      @frame_rate_upper_threshold = attrs[:frame_rate_upper_threshold]
+      @keyframe_upper_threshold = attrs[:keyframe_upper_threshold]
       @audio_bitrate_kbps = attrs[:audio_bitrate_kbps]
       @audio_channels = attrs[:audio_channels]
       @audio_sample_rate = attrs[:audio_sample_rate]
-      @max_bitrate_kbps = attrs[:max_bitrate_kbps]
-      @keyframe_period = attrs[:keyframe_period]
       @created_at = attrs[:created_at]
       @updated_at = attrs[:updated_at]
     end
@@ -40,16 +39,15 @@ module VzaarApi
         description: self.description,
         output_format: self.output_format,
         bitrate_kbps: self.bitrate_kbps,
+        max_bitrate_kbps: self.max_bitrate_kbps,
         long_dimension: self.long_dimension,
         video_codec: self.video_codec,
         profile: self.profile,
-        frame_rate: self.frame_rate,
-        keyframe: self.keyframe,
+        frame_rate_upper_threshold: self.frame_rate_upper_threshold,
+        keyframe_upper_threshold: self.keyframe_upper_threshold,
         audio_bitrate_kbps: self.audio_bitrate_kbps,
         audio_channels: self.audio_channels,
         audio_sample_rate: self.audio_sample_rate,
-        max_bitrate_kbps: self.max_bitrate_kbps,
-        keyframe_period: self.keyframe_period,
         created_at: self.created_at,
         updated_at: self.updated_at
       }
--- spec/vzaar_api/encoding_preset_spec.rb
+++ spec/vzaar_api/encoding_preset_spec.rb
@@ -10,22 +10,21 @@ module VzaarApi
         VCR.use_cassette('encoding_presets/find') do
           preset = described_class.find(4).to_hash
           expect(preset[:id]).to eq 4
-          expect(preset[:name]).to eq "SD"
-          expect(preset[:description]).to eq "540p"
+          expect(preset[:name]).to eq "LD"
+          expect(preset[:description]).to eq "270p"
           expect(preset[:output_format]).to eq "mp4"
-          expect(preset[:bitrate_kbps]).to eq 1944
-          expect(preset[:long_dimension]).to eq 960
+          expect(preset[:bitrate_kbps]).to eq 365
+          expect(preset[:max_bitrate_kbps]).to eq 500
+          expect(preset[:long_dimension]).to eq 480
           expect(preset[:video_codec]).to eq "libx264"
-          expect(preset[:profile]).to eq "main"
-          expect(preset[:frame_rate]).to eq "29.97"
-          expect(preset[:keyframe]).to eq 60
+          expect(preset[:profile]).to eq "baseline"
+          expect(preset[:frame_rate_upper_threshold]).to eq "29.97"
+          expect(preset[:keyframe_upper_threshold]).to eq 60
           expect(preset[:audio_bitrate_kbps]).to eq 128
           expect(preset[:audio_channels]).to eq 2
           expect(preset[:audio_sample_rate]).to eq 44100
-          expect(preset[:max_bitrate_kbps]).to eq nil
-          expect(preset[:keyframe_period]).to eq nil
-          expect(preset[:created_at]).to eq "2016-11-09T11:01:38.000Z"
-          expect(preset[:updated_at]).to eq "2016-11-09T11:01:38.000Z"
+          expect(preset[:created_at]).to eq "2016-10-24T12:36:47.000Z"
+          expect(preset[:updated_at]).to eq "2016-10-24T12:36:47.000Z"
         end
       end
     end
@@ -36,22 +35,21 @@ module VzaarApi
           VCR.use_cassette('encoding_presets/find') do
             preset = described_class.find(4)
             expect(preset.id).to eq 4
-            expect(preset.name).to eq "SD"
-            expect(preset.description).to eq "540p"
+            expect(preset.name).to eq "LD"
+            expect(preset.description).to eq "270p"
             expect(preset.output_format).to eq "mp4"
-            expect(preset.bitrate_kbps).to eq 1944
-            expect(preset.long_dimension).to eq 960
+            expect(preset.bitrate_kbps).to eq 365
+            expect(preset.max_bitrate_kbps).to eq 500
+            expect(preset.long_dimension).to eq 480
             expect(preset.video_codec).to eq "libx264"
-            expect(preset.profile).to eq "main"
-            expect(preset.frame_rate).to eq "29.97"
-            expect(preset.keyframe).to eq 60
+            expect(preset.profile).to eq "baseline"
+            expect(preset.frame_rate_upper_threshold).to eq "29.97"
+            expect(preset.keyframe_upper_threshold).to eq 60
             expect(preset.audio_bitrate_kbps).to eq 128
             expect(preset.audio_channels).to eq 2
             expect(preset.audio_sample_rate).to eq 44100
-            expect(preset.max_bitrate_kbps).to eq nil
-            expect(preset.keyframe_period).to eq nil
-            expect(preset.created_at).to eq "2016-11-09T11:01:38.000Z"
-            expect(preset.updated_at).to eq "2016-11-09T11:01:38.000Z"
+            expect(preset.created_at).to eq "2016-10-24T12:36:47.000Z"
+            expect(preset.updated_at).to eq "2016-10-24T12:36:47.000Z"
           end
         end
       end
@@ -71,7 +69,7 @@ module VzaarApi
         VCR.use_cassette('encoding_presets/each_item') do
           enum = described_class.each_item(per_page: 2)
           ids = enum.map { |preset| preset.id }
-          expect(ids).to match_array [1, 2, 3, 4, 5, 6, 7, 8, 9]
+          expect(ids).to match_array [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
         end
       end
     end
@@ -82,7 +80,7 @@ module VzaarApi
           pager = described_class.paginate(per_page: 1)
           pager.first
           ids = pager.collection.map { |preset| preset.id }
-          expect(ids).to match_array [4]
+          expect(ids).to match_array [9]
         end
       end
 
@@ -91,7 +89,7 @@ module VzaarApi
           pager = described_class.paginate(per_page: 1)
           pager.next
           ids = pager.collection.map { |preset| preset.id }
-          expect(ids).to match_array [3]
+          expect(ids).to match_array [2]
         end
       end
 
@@ -100,7 +98,7 @@ module VzaarApi
           pager = described_class.paginate(per_page: 1)
           pager.last
           ids = pager.collection.map { |preset| preset.id }
-          expect(ids).to match_array [1]
+          expect(ids).to match_array [11]
         end
       end
 
@@ -109,7 +107,7 @@ module VzaarApi
           pager = described_class.paginate(page: 4, per_page: 1)
           pager.previous
           ids = pager.collection.map { |preset| preset.id }
-          expect(ids).to match_array [2]
+          expect(ids).to match_array [3]
         end
       end
     end
