86726603b8ed71dcfa8f410f72da796573a89ee5
Dan Rees-Jones <dan@vzaar.com>
2017-04-27 12:30:11 +0100

    (DEV-632) fix failing spec

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,5 +1,12 @@
 module Helpers
 
+  def api_envs
+    @api_envs = begin
+      env = ENV["API_ENV"] || "development"
+      YAML.load_file("api_envs.yml")[env]
+    end
+  end
+
   def setup_auth!
     VzaarApi.auth_token = 'TUGXzNL17ypaougMYpR3'
     VzaarApi.client_id  = 'lair-tend72'
